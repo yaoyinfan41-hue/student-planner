d696f46ba3dfc2f07b77d0f982aa1c9d6791d566
yaoyinfan41-hue <yaoyinfan41@gmail.com>
2026-04-04 23:45:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿function save() {
+  const text = document.getElementById("diary").value;
+  localStorage.setItem("diary", text);
+}
+
+window.onload = function() {
+  const saved = localStorage.getItem("diary");
+  document.getElementById("diary").value = saved || "";
+}
