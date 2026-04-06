11f2e370beb894bccd2c501f943c79b421a9f8eb
yaoyinfan41-hue <yaoyinfan41@gmail.com>
2026-04-06 15:58:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const days = ["mon","tue","wed","thu","fri","sat","sun"];
+const days=["mon","tue","wed","thu","fri","sat","sun"];
+
 
 
 // 出席
@@ -39,21 +40,16 @@ const data={};
 
 for(let i=1;i<=6;i++){
 
+data["start"+i]=document.getElementById("start"+i)?.value;
+data["end"+i]=document.getElementById("end"+i)?.value;
+
 days.forEach(day=>{
 
 data[day+i]=document.getElementById(day+i)?.value;
-
-data[day+i+"_room"]=
-document.getElementById(day+i+"_room")?.value;
-
-data[day+i+"_memo"]=
-document.getElementById(day+i+"_memo")?.value;
-
-data[day+i+"_attend"]=
-document.getElementById(day+i+"_attend")?.textContent;
-
-data[day+i+"_absent"]=
-document.getElementById(day+i+"_absent")?.textContent;
+data[day+i+"_room"]=document.getElementById(day+i+"_room")?.value;
+data[day+i+"_memo"]=document.getElementById(day+i+"_memo")?.value;
+data[day+i+"_attend"]=document.getElementById(day+i+"_attend")?.textContent;
+data[day+i+"_absent"]=document.getElementById(day+i+"_absent")?.textContent;
 
 });
 
@@ -72,26 +68,25 @@ const data=JSON.parse(localStorage.getItem("timetable")||"{}");
 
 for(let i=1;i<=6;i++){
 
+document.getElementById("start"+i).value=data["start"+i]||"";
+document.getElementById("end"+i).value=data["end"+i]||"";
+
 days.forEach(day=>{
 
 if(document.getElementById(day+i))
 document.getElementById(day+i).value=data[day+i]||"";
 
 if(document.getElementById(day+i+"_room"))
-document.getElementById(day+i+"_room").value=
-data[day+i+"_room"]||"";
+document.getElementById(day+i+"_room").value=data[day+i+"_room"]||"";
 
 if(document.getElementById(day+i+"_memo"))
-document.getElementById(day+i+"_memo").value=
-data[day+i+"_memo"]||"";
+document.getElementById(day+i+"_memo").value=data[day+i+"_memo"]||"";
 
 if(document.getElementById(day+i+"_attend"))
-document.getElementById(day+i+"_attend").textContent=
-data[day+i+"_attend"]||"0";
+document.getElementById(day+i+"_attend").textContent=data[day+i+"_attend"]||"0";
 
 if(document.getElementById(day+i+"_absent"))
-document.getElementById(day+i+"_absent").textContent=
-data[day+i+"_absent"]||"0";
+document.getElementById(day+i+"_absent").textContent=data[day+i+"_absent"]||"0";
 
 });
 
@@ -100,14 +95,26 @@ data[day+i+"_absent"]||"0";
 }
 
 
-// 土日
-function toggleWeekend(){
-document.querySelectorAll(".weekend").forEach(el=>{
-el.style.display =
-(el.style.display==="none")?"":"none";
-});
+
+// 土曜
+function showSat(){
+document.querySelectorAll(".sat").forEach(el=>el.style.display="")
+}
+
+function hideSat(){
+document.querySelectorAll(".sat").forEach(el=>el.style.display="none")
 }
 
 
-// 初期読み込み
+// 日曜
+function showSun(){
+document.querySelectorAll(".sun").forEach(el=>el.style.display="")
+}
+
+function hideSun(){
+document.querySelectorAll(".sun").forEach(el=>el.style.display="none")
+}
+
+
+
 window.onload=loadTimetable;
