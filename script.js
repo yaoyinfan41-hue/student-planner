a81d9d7aaf7743cccc9b0870fdbff1a0fe2285a4
yaoyinfan41-hue <yaoyinfan41@gmail.com>
2026-04-06 16:41:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,9 +21,28 @@ if(el){
 data[day+i] = el.value;
 }
 
+// 教室
 const room = document.getElementById(day+i+"_room");
 if(room){
 data[day+i+"_room"] = room.value;
+}
+
+// メモ
+const memo = document.getElementById(day+i+"_memo");
+if(memo){
+data[day+i+"_memo"] = memo.value;
+}
+
+// 出席
+const attend = document.getElementById(day+i+"_attend");
+if(attend){
+data[day+i+"_attend"] = attend.textContent;
+}
+
+// 欠席
+const absent = document.getElementById(day+i+"_absent");
+if(absent){
+data[day+i+"_absent"] = absent.textContent;
 }
 
 });
@@ -60,15 +79,71 @@ if(el && data[day+i]){
 el.value = data[day+i];
 }
 
+// 教室
 const room = document.getElementById(day+i+"_room");
 if(room && data[day+i+"_room"]){
 room.value = data[day+i+"_room"];
+}
+
+// メモ
+const memo = document.getElementById(day+i+"_memo");
+if(memo && data[day+i+"_memo"]){
+memo.value = data[day+i+"_memo"];
+}
+
+// 出席
+const attend = document.getElementById(day+i+"_attend");
+if(attend && data[day+i+"_attend"]){
+attend.textContent = data[day+i+"_attend"];
+}
+
+// 欠席
+const absent = document.getElementById(day+i+"_absent");
+if(absent && data[day+i+"_absent"]){
+absent.textContent = data[day+i+"_absent"];
 }
 
 });
 
 }
 
+}
+
+
+
+// ========================
+// 出席
+// ========================
+
+function addAttend(id){
+const el=document.getElementById(id+"_attend");
+el.textContent=parseInt(el.textContent)+1;
+saveTimetable();
+}
+
+function subAttend(id){
+const el=document.getElementById(id+"_attend");
+let n=parseInt(el.textContent);
+if(n>0) el.textContent=n-1;
+saveTimetable();
+}
+
+
+// ========================
+// 欠席
+// ========================
+
+function addAbsent(id){
+const el=document.getElementById(id+"_absent");
+el.textContent=parseInt(el.textContent)+1;
+saveTimetable();
+}
+
+function subAbsent(id){
+const el=document.getElementById(id+"_absent");
+let n=parseInt(el.textContent);
+if(n>0) el.textContent=n-1;
+saveTimetable();
 }
 
 
@@ -98,31 +173,3 @@ el.style.display="none";
 // ========================
 
 window.onload = loadTimetable;
-
-// 出席
-function addAttend(id){
-const el=document.getElementById(id+"_attend");
-el.textContent=parseInt(el.textContent)+1;
-saveTimetable();
-}
-
-function subAttend(id){
-const el=document.getElementById(id+"_attend");
-let n=parseInt(el.textContent);
-if(n>0) el.textContent=n-1;
-saveTimetable();
-}
-
-// 欠席
-function addAbsent(id){
-const el=document.getElementById(id+"_absent");
-el.textContent=parseInt(el.textContent)+1;
-saveTimetable();
-}
-
-function subAbsent(id){
-const el=document.getElementById(id+"_absent");
-let n=parseInt(el.textContent);
-if(n>0) el.textContent=n-1;
-saveTimetable();
-}
